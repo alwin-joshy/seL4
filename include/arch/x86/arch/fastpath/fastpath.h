6cc3c0a890cfbed354df3a50b29134ddfcdb5214
Alwin Joshy <a.joshy@student.unsw.edu.au>
2022-01-19 15:03:13 +1100

    trivial: guarding exception fastpath fns
    Signed-off-by: Alwin Joshy <a.joshy@student.unsw.edu.au>

--- include/arch/x86/arch/fastpath/fastpath.h
+++ include/arch/x86/arch/fastpath/fastpath.h
@@ -21,11 +21,13 @@ NORETURN;
 void fastpath_call(word_t cptr, word_t r_msgInfo)
 NORETURN;
 
+#ifdef CONFIG_EXCEPTION_FASTPATH
 void vm_fault_slowpath(vm_fault_type_t type)
 NORETURN;
 
 void fastpath_vm_fault(vm_fault_type_t type)
 NORETURN;
+#endif
 
 #ifdef CONFIG_KERNEL_MCS
 void fastpath_reply_recv(word_t cptr, word_t r_msgInfo, word_t reply)
