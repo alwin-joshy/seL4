68416be326383a88da79f2943a39a75c6cb7de6d
Alwin Joshy <joshyalwin@gmail.com>
2024-01-16 17:53:20 +1100

    wip: removing duplicated code between aarch64 and aarch32
    Signed-off-by: Alwin Joshy <joshyalwin@gmail.com>

--- src/arch/arm/machine/debug.c
+++ src/arch/arm/machine/debug.c
@@ -3,3 +3,39 @@
  *
  * SPDX-License-Identifier: GPL-2.0-only
  */
+
+#ifdef ARM_BASE_CP14_SAVE_AND_RESTORE
+
+/** Mirrors Arch_initFpuContext.
+ *
+ * Zeroes out the BVR thread context and preloads reserved bit values from the
+ * control regs into the thread context so we can operate solely on the values
+ * cached in RAM in API calls, rather than retrieving the values from the
+ * coprocessor.
+ */
+void Arch_initBreakpointContext(user_context_t *uc) {
+  uc->breakpointState = armKSNullBreakpointState;
+}
+
+void loadAllDisabledBreakpointState(void) {
+  int i;
+
+  /* We basically just want to read-modify-write each reg to ensure its
+   * "ENABLE" bit is clear. We did preload the register context with the
+   * reserved values from the control registers, so we can read our
+   * initial values from either the coprocessor or the thread's register
+   * context.
+   *
+   * Both are perfectly fine, and the only discriminant factor is performance.
+   * I suspect that reading from RAM is faster than reading from the
+   * coprocessor, but I can't be sure.
+   */
+  for (i = 0; i < seL4_NumExclusiveBreakpoints; i++) {
+    writeBcrCp(i, readBcrCp(i) & ~DBGBCR_ENABLE);
+  }
+  for (i = 0; i < seL4_NumExclusiveWatchpoints; i++) {
+    writeWcrCp(i, readWcrCp(i) & ~DBGWCR_ENABLE);
+  }
+}
+
+#endif /* ARM_BASE_CP14_SAVE_AND_RESTORE */
